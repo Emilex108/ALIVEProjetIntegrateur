382e9c4a84f72d0b603a3b2b2c6969ed58c8d83e
Barbarian14 <45662940+Barbarian14@users.noreply.github.com>
2020-02-10 09:59:18 -0500

    sds

--- src/utillities/Mapping2D.java
+++ src/utillities/Mapping2D.java
@@ -1,5 +1,6 @@
 package utillities;
 
+import java.awt.Color;
 import java.awt.Dimension;
 import java.awt.Graphics;
 import java.awt.Graphics2D;
@@ -22,7 +23,11 @@ public class Mapping2D extends JFrame {
 	private double posY=0;
 	//with a speed of 110
 	private final double MAXSPEED = 0.4;
+	private final double CAR_WIDTH = 0.20;
+	private final int ABBERATE_DISTANCE = 200;
 	private double lastAngle=0;
+	
+	private Ellipse2D car;
 	
 	public Mapping2D() {
 		this.add(new DrawPanel());
@@ -45,7 +50,17 @@ public class Mapping2D extends JFrame {
 			double distance = MAXSPEED*timeS;
 			posX+= distance*Math.sin(angleRad);
 			posY+= distance*Math.cos(angleRad);
-			addPoint(posX, posY);
+			repaint();
+		}
+	}
+	
+	public void wallDetection(int distanceLeft, int ditanceCenter, int distanceRight, int angle) {
+		double posX;
+		double posY;
+		double angleRad = Math.toRadians(angle);
+		if(distanceLeft<ABBERATE_DISTANCE) {
+			posX = distanceLeft/100*Math.sin(angleRad);
+			posY = distanceLeft/100*Math.cos(angleRad);
 		}
 	}
 	
@@ -61,7 +76,15 @@ public class Mapping2D extends JFrame {
 			Graphics2D g2d = (Graphics2D) g;
 			DisplayModel display = new DisplayModel(getWidth(), getHeight(), 10);	
 			AffineTransform mat = display.getModel();
+		
+			
 			g2d.translate(this.getWidth()/2.0, this.getHeight()/2.0);
+			
+			car =  new Ellipse2D.Double(posX, posY, CAR_WIDTH, CAR_WIDTH);
+			g2d.setColor(Color.red);
+			g2d.fill(mat.createTransformedShape(car));
+			
+			g2d.setColor(Color.black);
 			for(Ellipse2D e : points) {
 				g2d.fill(mat.createTransformedShape(e));
 			}
