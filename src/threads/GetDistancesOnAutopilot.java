85c2f7a979aff84570d4f21112c86659bfd30892
Gagné <e1836636@cmaisonneuve.qc.ca>
2020-02-12 16:21:25 -0500

    ss

--- src/threads/GetDistancesOnAutopilot.java
+++ src/threads/GetDistancesOnAutopilot.java
@@ -6,19 +6,24 @@ import java.io.OutputStream;
 import org.jsoup.Jsoup;
 
 import aapplication.Application;
+import utilities.Mapping2D;
 
 public class GetDistancesOnAutopilot extends Thread{
 
 	InputStream inStream;
 	OutputStream outStream;
 	int angleSign, angle, nbHalfTurn;
+	long startTime, endTime;
+	Mapping2D map;
 
 	@Override
 	public void run() {
 		inStream = Application.getInStream();
 		outStream = Application.getOutStream();
+		map = new Mapping2D();
 		while(Application.getautoPilotOn()) {
 			try {
+				startTime = System.nanoTime();
 				outStream.write(103);
 				outStream.flush();
 				while(inStream.available()==0);
@@ -34,29 +39,10 @@ public class GetDistancesOnAutopilot extends Thread{
 				while(inStream.available()==0);
 				nbHalfTurn = Integer.parseInt(Jsoup.parse(inStream.read()+"").text());
 				System.out.println("Demi Tours : " + nbHalfTurn);
+				endTime = System.nanoTime()-startTime;
+				map.trackPosition(angle);
 			}catch(Exception e) {}
 		}
-	}
-
-	/**
-	 * @return the angleSign
-	 */
-	public int getAngleSign() {
-		return angleSign;
-	}
-
-	/**
-	 * @return the angle
-	 */
-	public int getAngle() {
-		return angle;
-	}
-
-	/**
-	 * @return the nbHalfTurn
-	 */
-	public int getNbHalfTurn() {
-		return nbHalfTurn;
 	}
 
 }
